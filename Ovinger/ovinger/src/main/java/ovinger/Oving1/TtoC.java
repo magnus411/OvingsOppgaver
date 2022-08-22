2003796f36c7b8364f8afb89457c3b53dbeef84a
Magnus411 <magnusgjerstad00@gmail.com>
2022-08-22 13:21:41 +0200

    Maven

---
+++ Ovinger/ovinger/src/main/java/ovinger/Oving1/TtoC.java
@@ -0,0 +1,47 @@
+package ovinger.Oving1;
+
+
+//Program for converting inces to cm
+
+
+class TtoC {
+
+    // Defining the function convert.
+    private Double convert(Double centi) {
+
+        //Logic 
+        //Converting inces to cm
+
+
+        //Defining that Inches is a dobule with value 2.54
+        Double tomme = 2.54;
+        //Definding that cm to inces is cm / inces
+        Double ctt = centi / tomme;
+
+        //Returning the converted value
+        return ctt;
+    }
+
+
+    // Creating a new array with the values 2.3, 1.2, 1.5, 10.2, 59.42.
+    private static Double[] dataSet = new Double[] {2.3, 1.2, 1.5, 10.2, 59.42};
+
+
+    // The main function.
+    public static void main(String[] args)  {
+
+        // Creating a new instance of the class TtoC.
+        TtoC converter = new TtoC();
+
+
+        
+
+        System.out.println(args[0]);
+
+        // Looping through the array and printing out the converted values.
+        for (int i = 0; i < dataSet.length; i++) {
+            System.out.println(converter.convert(dataSet[i]));
+
+        }
+    }
+}
