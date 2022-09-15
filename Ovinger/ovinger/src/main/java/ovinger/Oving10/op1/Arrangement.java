09f0f13128b9ebb861e368790e7bfa7218709020
Magnus411 <magnusgjerstad00@gmail.com>
2022-09-15 14:00:23 +0200

    Oving10

---
+++ Ovinger/ovinger/src/main/java/ovinger/Oving10/op1/Arrangement.java
@@ -0,0 +1,97 @@
+package ovinger.Oving10.op1;
+
+import java.util.ArrayList;
+
+public class Arrangement {
+
+    int nummer;
+    String navn;
+    String sted;
+    String arrangør;
+
+    enum Type {
+        KONSERT,
+        BARNETEATER,
+        TEATER,
+        FOREDRAG,
+    }
+
+    Type type;
+    String tidspunkt;
+    String dato;
+
+    public Arrangement() {
+
+    }
+
+    public Arrangement(int nummer, String navn, String sted, String tidspunkt, String arrangør) {
+        this.nummer = nummer;
+        this.navn = navn;
+        this.sted = sted;
+        // this.type = type;
+        this.tidspunkt = tidspunkt;
+
+        this.dato = tidspunkt.substring(0, 8);
+        this.arrangør = arrangør;
+
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getSted() {
+        return sted;
+    }
+
+    public void setSted(String sted) {
+        this.sted = sted;
+    }
+
+    public String getArrangør() {
+        return arrangør;
+    }
+
+    public void setArrangør(String arrangør) {
+        this.arrangør = arrangør;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getTidspunkt() {
+        return tidspunkt;
+    }
+
+    public void setTidspunkt(String tidspunkt) {
+        this.tidspunkt = tidspunkt;
+    }
+
+    public String getDato() {
+        return this.dato;
+    }
+
+    public String toString() {
+        return "Arrangement [nummer=" + nummer + ", navn=" + navn + ", sted=" + sted + ", tidspunkt= " + tidspunkt
+                + ", type=" + type + ", arrangør="
+                + arrangør + "]";
+    }
+
+}
---
+++ Ovinger/ovinger/src/main/java/ovinger/Oving10/op1/ArrangementRegister.java
@@ -0,0 +1,72 @@
+package ovinger.Oving10.op1;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class ArrangementRegister {
+
+    public ArrayList<Arrangement> Arr;
+
+    public ArrangementRegister() {
+
+        Arr = new ArrayList<Arrangement>();
+
+        Arrangement arr1 = new Arrangement(1, "Sondre", "Oslo", "202110141800", "Sondre");
+        Arrangement arr2 = new Arrangement(2, "Sondre", "Oslo", "202110121800", "Sondre");
+        Arrangement arr3 = new Arrangement(3, "Sondre", "Oslo", "202110131800", "Sondre");
+        Arr.add(arr1);
+        Arr.add(arr2);
+        Arr.add(arr3);
+
+    }
+
+    public void addArrangement(Arrangement a) {
+        Arr.add(a);
+    }
+
+    public void removeArrangement(Arrangement a) {
+        Arr.remove(a);
+    }
+
+    public void finnArrangement() {
+        for (int i = 0; i < Arr.size(); i++) {
+            System.out.println(Arr.get(i));
+        }
+    }
+
+    public void finnArrangementArrangør(String arrangør) {
+        Arr.stream().filter(e -> e.getArrangør().equals(arrangør)).forEach(e -> System.out.println(e));
+    }
+
+    public void finnArrangementDato(String dato) {
+        Arr.stream().filter(e -> e.getDato().equals(dato)).forEach(e -> System.out.println(e));
+    }
+
+    public void finnArrangementSted(String sted) {
+        Arr.stream().filter(e -> e.getSted().equals(sted)).forEach(e -> System.out.println(e));
+    }
+
+    public void finnArrangementType(String type) {
+        Arr.stream().filter(e -> e.getType().equals(type)).forEach(e -> System.out.println(e));
+    }
+
+    public void finnArrangementNavn(String navn) {
+        Arr.stream().filter(e -> e.getNavn().equals(navn)).forEach(e -> System.out.println(e));
+    }
+
+    public void finnArrangementMellom(String dato1, String dato2) {
+
+        ArrayList<Arrangement> sorter = new ArrayList<Arrangement>();
+
+        Arr.stream().filter(e -> e.getDato().compareTo(dato1) >= 0 && e.getDato().compareTo(dato2) <= 0)
+                .forEach(e -> sorter.add(e));
+
+        sorter.sort((e1, e2) -> e1.getDato().compareTo(e2.getDato()));
+        sorter.forEach(e -> System.out.println(e));
+    }
+
+    public static void main(String[] args) {
+        ArrangementRegister ar = new ArrangementRegister();
+        ar.finnArrangementMellom("20211011", "20211015");
+    }
+}
